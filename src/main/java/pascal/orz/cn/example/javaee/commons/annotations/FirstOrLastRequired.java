b2e089d5dd2216e4087360f4870fc94320c1e529
koduki <koduki@nklab.dev>
2025-05-21 05:20:53 +0900

    rename

---
+++ src/main/java/pascal/orz/cn/example/javaee/commons/annotations/FirstOrLastRequired.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pascal.orz.cn.example.javaee.commons.annotations;
+
+import pascal.orz.cn.example.javaee.commons.validators.FirstOrLastRequiredValidator;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import javax.validation.Payload;
+import pascal.orz.cn.example.javaee.commons.validators.FirstOrLastRequiredValidator;
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy={FirstOrLastRequiredValidator.class})
+public @interface FirstOrLastRequired {
+
+    String message() default "may not be empty2";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
