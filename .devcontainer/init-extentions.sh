55036ef3857e43ed819bc33b3234612b01d5d11c
koduki <koduki@nklab.dev>
2025-05-29 08:09:40 +0900

    Modify devcontainers

--- .devcontainer/init-extentions.sh
+++ .devcontainer/init-extentions.sh
@@ -1,2 +1,3 @@
 mkdir -p /home/vscode/.cursor-server/data/User/globalStorage/payara.payara-vscode/
 cp /workspace/.devcontainer/ext-config/payara/servers.json /home/vscode/.cursor-server/data/User/globalStorage/payara.payara-vscode/servers.json
+cp /workspace/.devcontainer/ext-config/payara/domain.xml /opt/payara/appserver/glassfish/domains/domain1/config/domain.xml
